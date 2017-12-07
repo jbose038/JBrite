ca223f37519886a3f6f62289a23612b5bfdd2992
jbose038 <31946465+jbose038@users.noreply.github.com>
2017-12-07 17:57:02 +0900

    test
    test

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path')
-const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 5000
 
 /* GET home page. */
 router.get('/', function(req, res, next) {

4b10ac46eecafe9d9a18079bf7f91b81e645c9cb
jbose038 <31946465+jbose038@users.noreply.github.com>
2017-12-07 18:06:31 +0900

    test
    test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'JBrite'});

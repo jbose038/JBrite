f27705e8a7c9500c51d42b28dff4d52ca2061184
jbose038 <31946465+jbose038@users.noreply.github.com>
2017-12-07 18:23:33 +0900

    test
    test

--- app.js
+++ app.js
@@ -20,11 +20,13 @@ var passportConfig = require('./lib/passport-config');
 var app = express();
 
 // view engine setup
+/*
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 if (app.get('env') === 'development') {
   app.locals.pretty = true;
 }
+*/
 
 app.locals.moment = require('moment');
 app.locals.querystring = require('querystring');
